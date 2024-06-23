329d79b2adc528c0cb10762430ae8e30ff8a9760
stahlgit <peter.stahl@betrian.cz>
2024-06-23 23:07:39 +0200

    FIX: basic bucket

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform{
-    backend "s3" {
-        bucket = "stahls-terraform-bucket"
-        key = "terraform.tfstate"
-        region = "eu-central-1"
-    }
-}
-
 provider "aws" {
     region = var.aws_region
 }
