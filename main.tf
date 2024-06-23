1b7991032c5c287ec51f7a88d9d9e541b0b08cbf
stahlgit <peter.stahl@betrian.cz>
2024-06-23 23:11:51 +0200

    ADD: Terraform data

--- main.tf
+++ main.tf
@@ -1,18 +1,37 @@
 provider "aws" {
-    region = var.aws_region
-}
-
-resource "aws_s3_bucket" "bucket" {
-    bucket = var.bucket_name
+  region = var.aws_region
 }
 
 resource "aws_instance" "web" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
 
-    tags = {
-        Name = "Terrform-EC2"
-    }
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
 
+terraform {
+  backend "s3" {
+    bucket = "stahls-terraform-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the instance"
+}
+
+variable "instance_type" {
+  description = "The instance type to use for the instance"
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance"
 }
